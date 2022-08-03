95de9926a7943a2fdaacec88dd044c1fd565fcce
wesyoung98 <107873633+wyoung98@users.noreply.github.com>
2022-08-03 17:37:39 -0500

    Changes to CRUD operations Reservations

--- Frontend/car-frontend/src/app/services/reservation.service.ts
+++ Frontend/car-frontend/src/app/services/reservation.service.ts
@@ -4,6 +4,12 @@ import { Reservation } from 'Reservation';
   providedIn: 'root'
 })
 export class ReservationService {
-
+  
   constructor() { }
+
+  createReservation(reservationID:number, start_date:Date, end_date:Date, status:boolean,carID:number,customerID:number){}
+  readReservation(reservationID:number){}
+  updateReservation(reservationID:number, start_date:Date, end_date:Date, status:boolean,carID:number,customerID:number){}
+  deleteReservation(reservationID:number){}
+
 }
