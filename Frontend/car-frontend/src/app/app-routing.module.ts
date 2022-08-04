cde986c185e87d27c8a5675d76452bd61bdd39fe
wesyoung98 <107873633+wyoung98@users.noreply.github.com>
2022-08-03 20:06:51 -0400

    Router Links Added

--- Frontend/car-frontend/src/app/app-routing.module.ts
+++ Frontend/car-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarCardsComponent } from './car-cards/car-cards.component';
+import { MyReservationsComponent } from './my-reservations/my-reservations.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'my-reservations', component: MyReservationsComponent},
+  {path: 'car-cards', component: CarCardsComponent},
+  {path: 'reservation', component: ReservationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
